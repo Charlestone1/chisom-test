ceed5d4ab3202bcb5d380e0859db295289838231
Charlestone1 <charlesorih007@gmail.com>
2025-03-19 11:02:25 +0100

    first commitss

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import JobListing from './components/JobListing'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <JobListing />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/JobListing.tsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+const JobListing = () => {
+
+    // const jobs = [
+    //     { id: 1, title: "Frontend Developer", company: "Tech Corp", location: "Remote", salary: "₦400,000" },
+    //     { id: 2, title: "Backend Engineer", company: "Dev Solutions", location: "Enugu", salary: "₦300,000" }
+    // ];
+
+
+    return (
+        <div>
+            <h2>JobListing</h2>
+
+            <p>Job list card goes here</p>
+        </div>
+    )
+}
+
+export default JobListing
